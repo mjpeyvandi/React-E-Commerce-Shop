98ca29663cac2620ac8fb941d11b1d63260f5b1a
Mohammadjavad Peyvandi <mohammadjp12@gmail.com>
2024-05-06 19:18:30 +0330

    get 4 new products

---
+++ src/services/getArrivals.js
@@ -0,0 +1,17 @@
+import supabase from "./supabase";
+
+export async function getArrivals() {
+  let { data, error } = await supabase
+    .from("products")
+    .select("*")
+    // Filter
+    .gt("created", "2024-01-01 00:00:00")
+    .gt("rate", "3.5")
+    .limit(4)
+
+    if(error){
+        console.log(error)
+    }
+
+    return data
+}
