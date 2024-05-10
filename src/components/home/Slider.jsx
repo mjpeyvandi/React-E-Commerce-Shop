39e7af977c2deac68630d7f98f080ea72ce9f8fd
mjpeyvandi <mohammadjp12@gmail.com>
2024-05-10 21:34:49 +0330

    Add component Slider

---
+++ src/components/home/Slider.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+
+import { IoArrowForward } from "@react-icons/all-files/io5/IoArrowForward";
+import { IoArrowBack } from "@react-icons/all-files/io5/IoArrowBack";
+
+import { getReviews } from "../../services/getReviews";
+import { CardReview } from "./CardReview";
+
+export const Slider = () => {
+    const [reviews, setReviews] = useState([])
+    const [currentIndex, setIndex] = useState(0)
+    
+    useEffect(()=>{
+        getReviews().then((data)=> setReviews(data))
+    },[])
+
+    function handleBackItem(){
+      if(currentIndex > 0){
+        setIndex(currentIndex => currentIndex - 3)
+      }
+    }
+
+    function handleNextItem(){
+      if(currentIndex < reviews.length - 3 ){
+        setIndex(currentIndex => currentIndex + 3)
+      }
+    }
+
+  return (
+    <div className="container mx-auto h-full flex flex-col justify-between items-center gap-10">
+      <div className="w-full h-1/6 flex flex-row justify-between items-center">
+        <h2 className="font-titr text-5xl">OUR HAPPY CUSTOMERS</h2>
+        <div className="flex flex-row justify-between items-center gap-3">
+          <IoArrowBack className="size-6 cursor-pointer" onClick={handleBackItem} />
+          <IoArrowForward className="size-6 cursor-pointer"  onClick={handleNextItem}/>
+        </div>
+      </div>
+      <div className="h-5/6 w-full flex flex-row justify-start items-center gap-5 overflow-hidden">
+        {
+          reviews.slice(currentIndex, currentIndex + 3)
+          .map((item)=>(
+            <CardReview rate={item.rate} name={item.name} comment={item.comment}/>
+          ))
+        }
+      </div>
+    </div>
+  );
+};
