639447ab083cbedc3f260c4a8a4e2784213ac508
Javad <mohammadjp12@gmail.com>
2024-12-17 19:37:40 +0330

    debug rate and star component

--- src/components/home/Rating.jsx
+++ src/components/home/Rating.jsx
@@ -5,30 +5,25 @@ export const Rating = ({ rate_Product, showNumber, sizeStar }) => {
   const [rate, setRating] = useState(0);
 
   useEffect(() => {
-    // اطمینان از اینکه مقدار rate_Product عدد است
     if (typeof rate_Product === "number" && !isNaN(rate_Product)) {
-      // از Math.ceil استفاده نمی‌کنیم، به دلیل نیاز به نیم ستاره
       setRating(rate_Product);
     }
   }, [rate_Product]);
 
-  const starsFilled = Math.floor(rate); // تعداد ستاره‌های پر شده
+  const starsFilled = Math.floor(rate); // تعداد ستاره‌های پر
   const hasHalfStar = rate % 1 >= 0.5;  // چک کردن نیم‌ستاره
 
   return (
     <div>
       <div className="flex flex-row justify-start items-center gap-1">
         {Array.from({ length: 5 }, (_, i) => {
-          let fill = i < starsFilled;
-          let halfStar = !fill && hasHalfStar && i === starsFilled;
-          return (
-            <Star
-              fill={fill}
-              key={i}
-              halfStar={halfStar}
-              size={sizeStar}
-            />
-          );
+          if (i < starsFilled) {
+            return <Star key={i} fill={true} size={sizeStar} />;
+          } else if (i === starsFilled && hasHalfStar) {
+            return <Star key={i} halfStar={true} size={sizeStar} />;
+          } else {
+            return <Star key={i} fill={false} size={sizeStar} />;
+          }
         })}
         {showNumber ? (
           <span className="font-satoshi-l text-sm xs:text-base">
--- src/components/home/Star.jsx
+++ src/components/home/Star.jsx
@@ -7,11 +7,11 @@ export const Star = ({ fill, halfStar, size }) => {
   return (
     <div>
       {fill ? (
-        <MdStar className={`${size ? `size-${size}` : 'size-5'}  text-star`} />
+        <MdStar className={`${size ? `size-${size}` : 'size-5'}  text-star drop-shadow-[0_0_2px_rgb(255,198,51)]`} />
       ) : halfStar ? (
-        <MdStarHalf className={`${size ? `size-${size}` : 'size-5'}  text-star`} />
+        <MdStarHalf className={`${size ? `size-${size}` : 'size-5'}  text-star drop-shadow-[0_0_2px_rgb(255,198,51)]`} />
       ) : (
-        <MdStar className={`${size ? `size-${size}` : 'size-5'}  text-white`} />
+        <MdStar className={`${size ? `size-${size}` : 'size-5'}  text-white drop-shadow-[0_0_2px_rgb(255,198,51)]`} />
       )}
     </div>
   );
