0fa9a82901e1b8a51bb6d5ae6d025aff75743da1
Javad <mohammadjp12@gmail.com>
2024-12-20 18:10:24 +0330

    checking online status user

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -5,19 +5,33 @@ import { Footer } from "./Footer";
 import { Drawer } from "./Drawer";
 
 export const AppLayout = ({ children }) => {
+  const isOffline = !navigator.onLine;
   const [Modal, setOp] = useState(false);
   const [showMenu, setShowMenu] = useState(false);
   return (
     <div className="w-full h-auto">
       {showMenu && (
-        <div className="fixed inset-0 bg-black h-full z-[100] opacity-50" onClick={()=> setShowMenu(false)}></div>
+        <div
+          className="fixed inset-0 bg-black h-full z-[100] opacity-50"
+          onClick={() => setShowMenu(false)}
+        ></div>
       )}
       <Drawer open={showMenu} setShowMenu={setShowMenu} />
       <header className="container bg-white mx-auto">
         <Navbar setOp={setOp} setShowMenu={setShowMenu} />
       </header>
 
-      <main className={`${Modal ? "opacity-10" : ""}`}>{children}</main>
+      <main className={`${Modal ? "opacity-10" : ""}`}>
+        {isOffline ? (
+          <div className="text-center h-72">
+            <p className="text-red-600 text-center h-72 flex items-center w-full justify-center">
+              You are offline. Please check your internet connection.
+            </p>
+          </div>
+        ) : 
+           children 
+        }
+      </main>
 
       <footer className={`${Modal ? "opacity-10" : ""}`}>
         <Footer />
